3166acbcc2f2769365f11bc9ea02e3a3a8d04972
Mirjam Petti <hantei@users.noreply.github.com>
2019-09-23 11:34:16 +0300

    tund 4 lisa

--- 04tund/addfilm.php
+++ 04tund/addfilm.php
@@ -1,4 +1,5 @@
 <?php
+// KODUS - sql päringud ja näidake veebilehel, nt 10 aastat vanad filmid, või ainult kindla aastaga jne + salvestada film title year jne muutujasse, ja kui midagi on puudu (nimi) siis salvestust ei tehta, error võiks tulla siis, uus muutuja mis sisaldab infot kas kõik läks hästi. salvestage valuesse kui salvestus ei õnnestu, ehk lisada value formi koos php koodiga
  //globaalsed muutujad on väljaspool funktsiooni
   require("../../../configVP.php");
   $userName = "Mirjam Petti";
@@ -8,7 +9,10 @@
   //var_dump($_POST);
   //kui on nuppu vajutatud
   if(isset($_POST["submitFilm"])){
-	saveFilmInfo($_POST["filmTitle"], $_POST["filmYear"], $_POST["filmDuration"], $_POST["filmGenre"], $_POST["filmCompany"], $_POST["filmDirector"]);
+	//salvestame ainult siis kui vähemalt pealkiri on olemas
+	if(!empty($_POST["filmTitle"])) {
+	  saveFilmInfo($_POST["filmTitle"], $_POST["filmYear"], $_POST["filmDuration"], $_POST["filmGenre"], $_POST["filmCompany"], $_POST["filmDirector"]);
+	}
   }
   
   //lisame lehe päise
@@ -35,7 +39,7 @@
 	<br>
 	<label>Filmi tootja: </label><input type="text" name="filmCompany">
 	<br>
-	<label>Filmi lavastaja: </label><input type="text" name="filmDirector">
+	<label>Filmi lavastaja: </label><input type="text" value="<?php echo $filmDirectorInserted; ?> name="filmDirector">
 	<br>
 	<input type="submit" value="Salvesta filmi info" name="submitFilm">
   </form>
