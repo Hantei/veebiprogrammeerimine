aaf370e688f0f08f9c8720fd7447c4fb6c7520a6
Mirjam Petti <hantei@users.noreply.github.com>
2019-09-30 12:02:18 +0300

    viienda tunni töö

---
+++ 05tund/header.php
@@ -0,0 +1,5 @@
+<?php
+$userName = "Mirjam Petti";
+$headerContent = '<!DOCTYPE html><html lang="et"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>' .$userName .' Mirjam Petti veebiprogrammeerimise harjutused</title></head>';
+  echo $headerContent;
+?>
